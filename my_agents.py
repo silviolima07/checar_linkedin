7f9e4125bb031ab68f0468a72f202de3c5642b35
Silvio Lima <silviolima07@gmail.com>
2024-09-20 14:07:34 -0300

    Removed filereadtool

--- my_agents.py
+++ my_agents.py
@@ -24,7 +24,7 @@ def criar_agente_revisor(modelo):
         ,
         llm=modelo,
         verbose=True,
-        memory=False
+        memory=False,
         tools=[]
     )
     return revisor_link    
