375db34c5d68ae37d400ec4b92562b1c592c7540
Silvio Lima <silviolima07@gmail.com>
2025-06-22 12:24:20 -0300

    Update

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+import sys
+import sqlite3
+
+__import__('pysqlite3')
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
 import pandas as pd
 import streamlit as st
 from crewai import Crew, Process
